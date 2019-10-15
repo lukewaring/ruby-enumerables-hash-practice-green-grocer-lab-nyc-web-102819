19ff2857e51c6cdac80adf0e5d687ac1aaa69c4e
Luke Waring <lukewaring@gmail.com>
2019-10-15 19:59:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,12 +21,13 @@ end
 def apply_coupons(cart, coupons)
   coupons.map do |coupon|
     item = coupon[:item]
+    item_with_coupon = cart["#{item} W/COUPON"]
     if cart[item]
-      if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
-        cart["#{item} W/COUPON"] = { price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num] }
+      if cart[item][:count] >= coupon[:num] && !item_with_coupon
+        item_with_coupon = { price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num] }
           cart[item][:count] -= coupon[:num]
-      elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
-        cart["#{item} W/COUPON"][:count] += coupon[:num]
+      elsif cart[item][:count] >= coupon[:num] && item_with_coupon
+        item_with_coupon[:count] += coupon[:num]
         cart[item][:count] -= coupon[:num]
       end
       
