d23ff7db135ed7ebd170301fea5aac4847b49b6d
Luke Waring <lukewaring@gmail.com>
2019-10-15 19:19:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,4 +46,5 @@ def checkout(cart, coupons)
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = applied_clearance(applied_coupons)
+  
 end
