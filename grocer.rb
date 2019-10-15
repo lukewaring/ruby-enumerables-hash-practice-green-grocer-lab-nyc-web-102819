60c21807bc38d9bcb84342a69b4792fc7407c86a
Luke Waring <lukewaring@gmail.com>
2019-10-15 18:28:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,47 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.map do |element_hash|
+    element_name = element_hash.keys[0]
+    
+    if final_hash.has_key?(element_name)
+      final_hash[element_name][:count] += 1 
+    else
+      final_hash[element_name] = {
+        price: element_hash[element_name][:price],
+        clearance: element_hash[element_name][:clearance],
+        count: 1 
+      }
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.map do |coupon|
+    item = coupon[:item]
+    if cart[item]
+      if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+        cart["#{item} W/COUPON"] = { price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num] }
+      elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
+        cart["#{item} W/COUPON"][:count] += coupon[:num]
+      end
+      cart[item][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  final_hash = {}
+  cart.map do |element_hash|
+    element_name = element_hash.keys[0]
+    
+    if  
+  end
 end
 
-def checkout(cart, coupons)
-  # code here
-end
+# def checkout(cart, coupons)
+#   # code here
+# end
